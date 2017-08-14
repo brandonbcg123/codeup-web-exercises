9c5aedb8cda208b3e386fb0524cadca594792047
brandongossen <brandongossen@yahoo.com>
2017-08-14 16:55:29 -0500

    Created an inline and external js html file.
    -Added the alert ("Welcome to my website!) to both inline and external files
    
    -Added the prompt asking the user for their favorite color to both inline and external files.
    
    Added the alert informing the user that their favorite color is my favorite color as well.

--- js/external.js
+++ js/external.js
@@ -1,3 +1,9 @@
 "use strict";
 
 console.log("Hello from external JavaScript");
+
+var welcome = alert("Welcome to my Website!")
+
+var favoriteColor = prompt("What is your favorite color?")
+
+alert("Awesome, " + favoriteColor + " is my favorite color too!")
