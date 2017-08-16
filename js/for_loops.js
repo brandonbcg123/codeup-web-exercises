31511e85e3fbde3f391fa9258030dcbb5cae91ce
brandongossen <brandongossen@yahoo.com>
2017-08-16 17:26:44 -0500

    Finished Random Odd or Even Number Exercise
    -Added a random number generator that will only accept numbers from 20 to 200
    
    -Created a for loop to make sure no more than 10 numbers are generated.
    
    -Created an if/else statement with conditions that used a modular condition to determine which 10 numbers generated were even and which ones were odd and console.log all of them

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,10 +3,19 @@
 //For Loop Exercises
 
 //Multiplication Table Exercise
-var numberBetween1_10 = parseInt(prompt("Please enter a number between 1 and 10."));
+// var numberBetween1_10 = parseInt(prompt("Please enter a number between 1 and 10."));
+//
+// for (var i = 1; i <= 10; i++) {
+//     var total = i * numberBetween1_10;
+//     console.log(numberBetween1_10 + "x" + i + "=" + total);
+// }
 
+//Random Odd or Even 10 Number Exercise
 for (var i = 1; i <= 10; i++) {
-    var total = i * numberBetween1_10;
-    console.log(numberBetween1_10 + "x" + i + "=" + total);
+    var randomNumber = Math.floor(Math.random() * 180) + 20;
+    if (randomNumber % 2 === 0) {
+        console.log(randomNumber + " is even")
+    } else {
+        console.log(randomNumber + " is odd")
+    }
 }
-
