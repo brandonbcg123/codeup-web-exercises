eb36c9688a8ece376a4ec5538d69c88e166d96d7
brandongossen <brandongossen@yahoo.com>
2017-08-16 17:11:37 -0500

    Finished Multiplication Table (Added parseInt before prompt)
    -Made a for loop with conditions that stopped the multiplication of the number the user put in once it reached 10

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,9 +1,12 @@
 "use strict";
 
+//For Loop Exercises
 
-var numberBetween1_10 = prompt("Please enter a number between 1 and 10.");
+//Multiplication Table Exercise
+var numberBetween1_10 = parseInt(prompt("Please enter a number between 1 and 10."));
 
 for (var i = 1; i <= 10; i++) {
     var total = i * numberBetween1_10;
     console.log(numberBetween1_10 + "x" + i + "=" + total);
 }
+
