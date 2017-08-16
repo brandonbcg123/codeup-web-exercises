79d9b7dbbf5b6e6467e1e7acc8601987f80b768b
brandongossen <brandongossen@yahoo.com>
2017-08-16 15:40:02 -0500

    Finished Ice Cream Cone Exercise
    -Added a do-while lopp with an if/else statement inside it that had the condition where all the cones left could not be less than all the cones that could be sold to each client.
    
    -The While part of the do-while loop has a condition where all the cones left have to be more than 0...once it reaches 0 no more cones can be sold because of the if if statement inside the do part of the do-while loop.

--- js/while.js
+++ js/while.js
@@ -1,8 +1,32 @@
 "use strict"
 
+//While Loop Exercises
+
+
+//Multiples of 2 exercise
 var multiplesOf2 = 1;
 
 while (multiplesOf2 < 65536) {
     multiplesOf2 *= 2;
     console.log(multiplesOf2);
 }
+
+
+//Do-While Exercise
+//Ice Cream Cone Seller Exercise
+var allCones = Math.floor(Math.random() * 50) + 50;
+
+
+console.log("The total amount of cones to sell is: " + allCones);
+
+do {
+    var cones = Math.floor(Math.random() * 5) + 1;
+    if (allCones >= cones) {
+        console.log(cones + " cones sold...");
+        console.log((allCones -= cones) + " remaining.");
+    } else {
+        console.log("Cannot sell " + cones + " I only have " + allCones + " left.");
+    }
+} while (allCones > 0);
+
+console.log("Yay! I sold all the cones!")
