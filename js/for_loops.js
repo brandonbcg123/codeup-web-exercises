9b614988a75bc313c4d5a3c1b482b077c4365587
brandongossen <brandongossen@yahoo.com>
2017-08-16 18:58:37 -0500

    Edited the Multiplication Table Exercise
    -added an additional For loop with an if statement that prevents the user to enter a number lower than 1, higher than 0, or input something that was not a number at all.

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,13 +5,19 @@
 //Multiplication Table Exercise
 var numberBetween1_10 = parseInt(prompt("Please enter a number between 1 and 10."));
 
+for (var a = 1; a <= 10; a++) {
+    if (numberBetween1_10 <= 0 || numberBetween1_10 > 10 || isNaN(numberBetween1_10) || numberBetween1_10 === null) {
+        numberBetween1_10 = parseInt(prompt("Please enter a number between 1 and 10."));
+    }
+}
+
 for (var i = 1; i <= 10; i++) {
     var total = i * numberBetween1_10;
     console.log(numberBetween1_10 + "x" + i + "=" + total);
 }
 
 //Random Odd or Even 10 Number Exercise
-for (var i = 1; i <= 10; i++) {
+for (var e = 1; e <= 10; e++) {
     var randomNumber = Math.floor(Math.random() * 180) + 20;
     if (randomNumber % 2 === 0) {
         console.log(randomNumber + " is even")
