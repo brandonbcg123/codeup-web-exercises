07efd3ec13edbb2995ece7995f3fe7b6ef0cc217
brandongossen <brandongossen@yahoo.com>
2017-08-16 17:56:59 -0500

    Uncommented the comments so all the answers would work.

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,12 +3,12 @@
 //For Loop Exercises
 
 //Multiplication Table Exercise
-// var numberBetween1_10 = parseInt(prompt("Please enter a number between 1 and 10."));
-//
-// for (var i = 1; i <= 10; i++) {
-//     var total = i * numberBetween1_10;
-//     console.log(numberBetween1_10 + "x" + i + "=" + total);
-// }
+var numberBetween1_10 = parseInt(prompt("Please enter a number between 1 and 10."));
+
+for (var i = 1; i <= 10; i++) {
+    var total = i * numberBetween1_10;
+    console.log(numberBetween1_10 + "x" + i + "=" + total);
+}
 
 //Random Odd or Even 10 Number Exercise
 for (var i = 1; i <= 10; i++) {
