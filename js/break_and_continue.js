4b27eb5e94ba3c228f7fd91fcddc302e2af229e4
brandongossen <brandongossen@yahoo.com>
2017-08-17 10:56:29 -0500

    Changed my while loop to a Do-While Loop so it would remove duplication.

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -5,39 +5,9 @@ console.log("Skipping numbers:");
 
 var isOdd = Number(prompt("Please insert an odd number between 1 and 50"));
 //What thing are not a valid input: Greater than 50, Less than 1, Even numbers, and NaN
-while (isOdd % 2 === 0) {
-    if (isOdd === "") {
-        break;
-    }
-    alert("The value you entered was not odd.");
+do {
     isOdd = Number(prompt("Please insert an odd number between 1 and 50."));
-}
-
-while (isOdd > 51) {
-    if (isOdd === "") {
-        break;
-    }
-    alert("The value you entered was not 50 or lower.");
-    isOdd = Number(prompt("Please insert a number between 1 and 50"));
-}
-
-while (isOdd < 0) {
-    if (isOdd === "") {
-        break;
-    }
-    alert("The value you entered was not above or 0.");
-    isOdd = Number(prompt("Please insert a number between 1 and 50."));
-}
-
-while (isNaN(isOdd)) {
-    if (isOdd === "") {
-        break;
-    }
-    alert("The value you entered was not an actual number.");
-    isOdd = Number(prompt("Please insert a number between 1 and 50."));
-}
-
-//What is the exercise asking for me to do: Once the user picks an odd number in between 1 and 50 console.log that number a a skipping number, console.log all the odd number between 1 and 50, the odd number the user picked needs to display a different message showing that it should be skipped.
+} while(isOdd % 2 === 0 || isOdd > 50 || isOdd < 0 || isNaN(isOdd));
 
 console.log("Number to skip is: " + isOdd);
 
