6872388836c08833ad5919974790e339c4309a59
brandongossen <brandongossen@yahoo.com>
2017-08-18 14:52:13 -0500

    2nd Commit
    -Logged a statement that displays the number of elements in the array via the .length function

--- js/iterating.js
+++ js/iterating.js
@@ -7,5 +7,10 @@
 
     // TODO: Create a log statement that will log the number of elements in the names array.
 
+    console.log("There are " + names.length + " names in the array.");
+
     // TODO: Create log statements that will print each of the names array elements individually.
+
+
+
 })();
