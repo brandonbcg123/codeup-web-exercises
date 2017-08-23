3a3da4bf56490a75f8ed60d49d9fe2c2cdfec828
brandongossen <brandongossen@yahoo.com>
2017-08-23 15:38:33 -0500

    Completed circle Math Object Exercise with additional comments added
    -Created a variable are and used Math.PI * Math.pow  to store in it
    
    -Created an if/else statement to show results when not round and when not rounded.
    
    -Changed the radius to 5 by circle.radius = 5.

--- js/circle.js
+++ js/circle.js
@@ -9,7 +9,7 @@
             // TODO: complete this method
             // hint: area = pi * radius^2
             var area = Math.PI * Math.pow(this.radius, 2);
-            //this replaces circle object, but it can be replaced by circle.radius as long as it is inside the scope. If not inside scope then have to do circle.radius
+            //this replaces circle object, but it can be replaced by circle.radius as long as it is inside the scope. If not inside scope then have to do circle.radius...its better to us this.radius in this situation because someone can change the variable name if they wanted to, so 'this' will always refer to the variabl
             return area; // TODO: return the proper value
         },
 
@@ -19,12 +19,12 @@
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
-            if (doRounding === true) {
+            if (doRounding) {
                 var roundedArea= Math.round(circle.getArea());
             } else {
                 roundedArea = circle.getArea();
             }
-
+            //circle can also be this.getArea in this case
             console.log("Area of a circle with radius: " + this.radius + ", is: " + roundedArea);
         }
     };
@@ -38,6 +38,7 @@
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
     circle.radius = 5;
+    //don't use this.radius here because it is now outside the object (The object I'm referring to is circle)
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
