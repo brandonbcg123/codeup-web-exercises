954856381a5f5c96b7edb13152688aa1285c7f48
brandongossen <brandongossen@yahoo.com>
2017-08-23 11:04:24 -0500

    Completed circle Math Object Exercise
    -Created a variable are and used Math.PI * Math.pow  to store in it
    
    -Created an if/else statement to show results when not round and when not rounded.
    
    -Changed the radius to 5 by circle.radius = 5.

--- js/circle.js
+++ js/circle.js
@@ -1,4 +1,4 @@
-(function() {
+// (function () {
     "use strict";
 
     // create a circle object
@@ -8,17 +8,24 @@
         getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
-
-            return; // TODO: return the proper value
+            var area = Math.PI * Math.pow(this.radius, 2);
+            //this replaces circle object, but it can be replaced by circle.radius as long as it is inside the scope. If not inside scope then have to do circle.radius
+            return area; // TODO: return the proper value
         },
+
 
         logInfo: function (doRounding) {
             // TODO: complete this method.
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
+            if (doRounding === true) {
+                var roundedArea= Math.round(circle.getArea());
+            } else {
+                roundedArea = circle.getArea();
+            }
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log("Area of a circle with radius: " + this.radius + ", is: " + roundedArea);
         }
     };
 
@@ -30,10 +37,10 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
-
+    circle.radius = 5;
     // log info about the circle
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
     circle.logInfo(true);
-})();
+// })();
