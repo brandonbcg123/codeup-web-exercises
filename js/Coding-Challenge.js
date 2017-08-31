c78e0f045b422599ee3e89b61d4562dfdebc169a
brandongossen <brandongossen@yahoo.com>
2017-08-31 11:57:17 -0500

    Accidental Comment
    -This file is just from the Geekdom coding challenge.

--- js/Coding-Challenge.js
+++ js/Coding-Challenge.js
@@ -1,31 +1,31 @@
-1.
-
-var a = 3;
-var b = -7;
-var c = 15;
-var number = a * b * c;
-
-if (number > 0) {
-    alert("+");
-    console.log("+");
-} else {
-    alert("-");
-    console.log("-");
-}
-
-2.
-
-var numberArray = [0, -1, 4];
-console.log(numberArray.sort());
-//if/else 222
-
-3.
-
-var temperatureFarenheit;
-var temperatureCelcius;
-function Temperature (temperature) {
-    var temperatureFarenheit = temperatureCelcius + 32;
-     return temperatureIsFarenheit;
-    var temperatureCelcius = temperatureFarenheit - 32;
-    return temperatureCelcius;
-}
+// 1.
+//
+// var a = 3;
+// var b = -7;
+// var c = 15;
+// var number = a * b * c;
+//
+// if (number > 0) {
+//     alert("+");
+//     console.log("+");
+// } else {
+//     alert("-");
+//     console.log("-");
+// }
+//
+// 2.
+//
+// var numberArray = [0, -1, 4];
+// console.log(numberArray.sort());
+// //if/else 222
+//
+// 3.
+//
+// var temperatureFarenheit;
+// var temperatureCelcius;
+// function Temperature (temperature) {
+//     var temperatureFarenheit = temperatureCelcius + 32;
+//      return temperatureIsFarenheit;
+//     var temperatureCelcius = temperatureFarenheit - 32;
+//     return temperatureCelcius;
+// }
