513c0e267b6e37a5e77ed07abb5e3f6a14f2c236
brandongossen <brandongossen@yahoo.com>
2017-09-12 12:39:35 -0500

    Promises 2nd Commit
    -generate a personal access token

--- js/promises.js
+++ js/promises.js
@@ -12,4 +12,8 @@
         wait(1000).then(() => console.log('You\'ll see this after 1 second'));
         wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
 
+
+        //Exercise Part 2: Problem 1
+
+    fetch(url, {headers: {"Authorization": "token 245052d97e34051ce5063e35f70517c9cfe1d207"}})
 })();
